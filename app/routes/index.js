71f80ab728bb940ac505bf3f391f6f2e117a5c95
“Anthony <nortonanthonya@gmail.com>
2018-06-13 02:14:54 -0600

    Add some api stuff

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,5 @@
-const noteRoutes = require('./note_routes');
+const noteRoutes = require('./note_routes')
 
 module.exports = (app, db) => {
-    console.log('./index.js');
-    noteRoutes(app, db);
+  noteRoutes(app, db)
 }
--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-require('./env-load');
-
-const app = express();
-
-const PORT = process.env.PORT || 8000;
-
-debugger;
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
+const dbConfig = require('./app/config/db')
+const assert = require('assert')
+const app = express()
+const PORT = process.env.PORT || 8000
 
 // Set app up to parse body of requests with bodyParser
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // ROUTES
-require('./app/routes')(app, {});
+assert.ok(typeof dbConfig.url === 'string', 'Database URI must be a string.')
+MongoClient.connect(
+  dbConfig.url,
+  (err, database) => {
+    assert.equal(null, err)
+    console.log('Successfully connected the database.')
+    const db = database.db(dbConfig.name)
+    require('./app/routes')(app, db)
 
-app.get('/', (req, res) => res.send('Hi (get request)'))
-app.post('/', (req, res) => res.send('Hi (post request)'))
-app.put('/', (req, res) => res.send('Hi (put request)'))
-app.delete('/', (req, res) => res.send('Hi (delete request)'))
-
-
-app.listen(PORT, () => {
-    console.log(`\nServer now listening on port ${PORT}.\n`);
-})
+    app.listen(PORT, () => {
+      console.log(`\nServer now listening on port ${PORT}.\n`)
+    })
+  },
+)
